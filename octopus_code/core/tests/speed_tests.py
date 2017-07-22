5d701c2d5e421f9453d620402f7feed525a60214
Aaron Fishman <uk.afishman@gmail.com>
2017-07-22 14:52:51 +0100

    logging framerates to file

--- octopus_code/core/tests/speed_tests.py
+++ octopus_code/core/tests/speed_tests.py
@@ -3,11 +3,14 @@ import core.octopus.layouts.octopus as octopus
 import core.octopus.opc
 
 import time
+import csv
 
 import numpy as np
 
 from core.octopus.patterns.rpcTestPattern import RpcTestPattern
 from core.octopus.patterns.shambalaPattern import ShambalaPattern
+
+import matplotlib.pyplot as plt
 
 # Standard library imports...
 from mock import patch, MagicMock
@@ -15,12 +18,12 @@ from mock import patch, MagicMock
 
 Testopus = "./core/tests/test_octopus.json" 
 
+Test_File = "./core/tests/test_data.csv"
 
 
 
 def speed_test(pattern, run_time=5):
-    print pattern.__class__.__name__
-
+    print "Speed testing", pattern.__class__.__name__
 
     pattern_generator = pg.PatternGenerator(octopus.ImportOctopus(Testopus), 
         framerate=20,
@@ -30,24 +33,53 @@ def speed_test(pattern, run_time=5):
     pattern_generator.patterns = [pattern]
 
     run_start = time.time()
+
+    test_file = open(Test_File, "w")
+    try: 
+        while time.time() - run_start < run_time:
+            print "\r", int(time.time() - run_start), "/", run_time,
+
+            loop_start = time.time()
+
+            pattern_generator.update()
+
+            loop_time = time.time() - loop_start
+            rate = 1.0/loop_time
+
+            #Log data
+            test_file.write(str(rate) + '\n')
+
+    except Exception as err:
+        raise err
+
+    finally:
+        test_file.close()
+
+    rates = load_csv(Test_File)
+
+    plt.plot(rates)
+    plt.show()
+
+
+    # mean_rate = np.mean(rates)
+    # min_rate = np.min(rates)
+    # max_rate = np.max(rates)
+
+    # print "Mean Rate", mean_rate
+    # print "Min Rate", min_rate
+    # print "Max Rate", max_rate
+
+def load_csv(filename):
     rates = []
+    with open(filename, 'rb') as csvfile:
+        csvreader = csv.reader(csvfile, delimiter=',')
+        for row in csvreader:
+            rates.append(float(row[0]))
 
-    while time.time() - run_start < run_time:
-        loop_start = time.time()
+    return np.array(rates)
 
-        pattern_generator.update()
 
-        loop_time = time.time() - loop_start
-        rate = 1.0/loop_time
-        rates.append(rate)
 
-    mean_rate = np.mean(rates)
-    min_rate = np.min(rates)
-    max_rate = np.max(rates)
-
-    print "Mean Rate", mean_rate
-    print "Min Rate", min_rate
-    print "Max Rate", max_rate
 
 
 if __name__ == '__main__':
