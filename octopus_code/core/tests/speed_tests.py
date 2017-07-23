19c56eaf60f00a58cc66b2f0fc68072c9aa53a32
Aaron Fishman <uk.afishman@gmail.com>
2017-07-23 14:07:08 +0100

    logging test data into csv

--- octopus_code/core/tests/speed_tests.py
+++ octopus_code/core/tests/speed_tests.py
@@ -2,6 +2,9 @@ import core.octopus.patternGenerator as pg
 import core.octopus.layouts.octopus as octopus
 import core.octopus.opc
 
+import os
+
+import psutil
 import time
 import csv
 
@@ -13,16 +16,17 @@ from core.octopus.patterns.shambalaPattern import ShambalaPattern
 import matplotlib.pyplot as plt
 
 # Standard library imports...
-from mock import patch, MagicMock
+#from mock import patch, MagicMock
+import mock_opc_server
 
 
 Testopus = "./core/tests/test_octopus.json" 
-
 Test_File = "./core/tests/test_data.csv"
 
 
 
-def speed_test(pattern, run_time=5):
+
+def speed_test(pattern, run_time=10):
     print "Speed testing", pattern.__class__.__name__
 
     pattern_generator = pg.PatternGenerator(octopus.ImportOctopus(Testopus), 
@@ -34,10 +38,14 @@ def speed_test(pattern, run_time=5):
 
     run_start = time.time()
 
+    process = psutil.Process(os.getpid())
+
     test_file = open(Test_File, "w")
+
+    # Run the Pattern for a bit and log data
     try: 
         while time.time() - run_start < run_time:
-            print "\r", int(time.time() - run_start), "/", run_time,
+            #print int(time.time() - run_start), "/", run_time
 
             loop_start = time.time()
 
@@ -46,8 +54,13 @@ def speed_test(pattern, run_time=5):
             loop_time = time.time() - loop_start
             rate = 1.0/loop_time
 
+            mem = process.memory_percent()
+            cpu = psutil.cpu_percent(interval=None)
+
             #Log data
-            test_file.write(str(rate) + '\n')
+            data = [time.time(), rate, mem, cpu]
+            test_file.write(",".join([str(x) for x in data]) + '\n')
+
 
     except Exception as err:
         raise err
@@ -55,38 +68,29 @@ def speed_test(pattern, run_time=5):
     finally:
         test_file.close()
 
-    rates = load_csv(Test_File)
+    plot_results(Test_File)
 
-    plt.plot(rates)
+
+def plot_results(filepath):
+    t, rates, mems, cpus = load_csv(filepath)
+
+    plt.plot(t, mems)
     plt.show()
 
-
-    # mean_rate = np.mean(rates)
-    # min_rate = np.min(rates)
-    # max_rate = np.max(rates)
-
-    # print "Mean Rate", mean_rate
-    # print "Min Rate", min_rate
-    # print "Max Rate", max_rate
-
 def load_csv(filename):
+    t = []
     rates = []
+    mem = []
+    cpu = []
     with open(filename, 'rb') as csvfile:
         csvreader = csv.reader(csvfile, delimiter=',')
         for row in csvreader:
-            rates.append(float(row[0]))
+            t.append(float(row[0]))
+            rates.append(float(row[1]))
+            mem.append(float(row[2]))
+            cpu.append(float(row[3]))
 
-    return np.array(rates)
-
-
-
-
+    return t, rates, mem, cpu
 
 if __name__ == '__main__':
-    patcher = patch('core.octopus.opc.Client')
-    opc_mock = patcher.start()
-    opc_mock.can_connect = MagicMock(return_value=True)
-    opc_mock.put_pixels = MagicMock()
-
-
     speed_test(ShambalaPattern())
