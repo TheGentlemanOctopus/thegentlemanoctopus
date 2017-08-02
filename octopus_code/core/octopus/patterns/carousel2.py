784e67e819eefdf400c3db2fb11054ec7e8cd2ce
Aaron Fishman <uk.afishman@gmail.com>
2017-08-02 12:04:33 +0100

    angular velocity

--- octopus_code/core/octopus/patterns/carousel2.py
+++ octopus_code/core/octopus/patterns/carousel2.py
@@ -3,29 +3,55 @@ import time
 from core.octopus.layouts.octopusLayout import OctopusLayout
 import numpy as np
 import colorsys
+import time
 
 class Carousel2(Pattern):
     # Speed is in degress per second
     def __init__(self, speed=180):
         self.register_param("hue_offset", 0, 1, 0.5)
-        self.register_param("hue_sweep", 0, 1, 0.5)
+        self.register_param("hue_sweep", 0, 1, 0.2)
 
-        self.register_param("saturation", 0, 1, 0.5)
+        self.register_param("saturation", 0, 1, 1)
         self.register_param("value", 0, 1, 0.8)
+
+        # Deg per second
+        self.register_param("ang_velocity", 0, 300, 150)
+
+        self.t = time.time()
 
     # Reset tings
     def on_pattern_select(self, octopus):
         self.octopus = octopus.clone()
+        self.dt()
+
+    # Time elapsed since last call to dt
+    def dt(self):
+        # Rotational
+        current_time = time.time()
+        dt = current_time - self.t
+        self.t = current_time
+
+        return dt
+
 
     # TODO: Phase shift on eq data
     def next_frame(self, octopus, data):
         pixels = self.octopus.pixels()
+        dt = self.dt()
 
+        #Get polar coordinates
         polar = np.array([pixel.polar_location() for pixel in pixels])
 
         r = polar[:,0]
         phi = polar[:,1]
 
+        #Go rotational
+        d_phi = dt * self.ang_velocity
+        for pixel in pixels:
+            pixel.rotate(d_phi)
+
+
+        #Set HSV
         h = self.hue_offset + 0.5*self.hue_sweep*np.sin(phi)
 
         s = self.saturation*np.ones(len(pixels))
