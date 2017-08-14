d290dd3a61daad95d6b03ad78b4adb4fdf7b4269
Aaron Fishman <uk.afishman@gmail.com>
2017-08-14 21:17:02 +0100

    BUGFIX: hue mod

--- octopus_code/core/octopus/patterns/carouselPattern.py
+++ octopus_code/core/octopus/patterns/carouselPattern.py
@@ -7,17 +7,14 @@ import time
 
 class CarouselPattern(Pattern):
     # Speed is in degress per second
-    def __init__(self, speed=180):
-        # self.register_param("hue_offset", 0, 1, 0.7)
-        self.hue_offset = 0
-
-        self.register_param("hue_sweep", 0, 1, 0.3)
+    def __init__(self):
+        self.register_param("hue_sweep", 0, 1, 0.2)
 
         self.register_param("saturation", 0, 1, 1)
         self.register_param("value", 0, 1, 0.8)
 
-        self.register_param("speed_gradient", -300, 300, 80)
-        self.register_param("speed_offset", -100, 100, -5)
+        self.register_param("speed_gradient", -300, 300, 100)
+        self.register_param("speed_offset", -100, 100, 5)
 
         self.register_param("hue_rate", 0, 1, 0.1)
 
@@ -53,7 +50,7 @@ class CarouselPattern(Pattern):
         r = np.linalg.norm(locations, axis=1)
         phi = np.arctan2(y, x)
 
-        self.hue_offset = (self.hue_offset + dt*data.eq[3]*self.hue_rate) % 1
+        self.hue_offset = (dt*data.eq[3]*self.hue_rate) % 1
 
         #Go rotational
         angular_velocity = self.speed_gradient*r + self.speed_offset
@@ -69,10 +66,10 @@ class CarouselPattern(Pattern):
             pixels[i].location = np.array([new_x[i], new_y[i], z[i]])
 
         #Set HSV
-        h = self.hue_offset + 0.5*self.hue_sweep*np.sin(phi)
+        h = (self.hue_offset + 0.5*self.hue_sweep*np.sin(phi)) % 1 
         s = self.saturation*np.ones(len(pixels))
         v = self.value*np.ones(len(pixels))
-        
+
         octopus_pixels = octopus.pixels()
         for i in range(len(pixels)):
             rgb = colorsys.hsv_to_rgb(h[i], s[i], v[i])
